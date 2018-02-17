1f6cdd35d1391b1581df82cad25c81f330c59296
mallowigi <heliosaian@gmail.com>
2018-02-17 14:13:02 +0200

    fix error in mac

--- src/ch/dasoft/iconviewer/ImageIconProvider.java
+++ src/ch/dasoft/iconviewer/ImageIconProvider.java
@@ -1,9 +1,12 @@
 package ch.dasoft.iconviewer;
 
 import com.intellij.ide.IconProvider;
+import com.intellij.openapi.util.IconLoader;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.util.IconUtil;
 import com.intellij.util.ImageLoader;
+import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -15,32 +18,37 @@ import java.io.FileNotFoundException;
 
 /**
  * Created by David Sommer on 19.05.17.
+ *
  * @author davidsommer
  */
-public class ImageIconProvider  extends IconProvider {
+public class ImageIconProvider extends IconProvider {
 
-    private static final int IMG_WIDTH  = 16;
-    private static final int IMG_HEIGHT = 16;
+  private static final int IMG_WIDTH = JBUI.scale(16);
+  private static final int IMG_HEIGHT = JBUI.scale(16);
 
-    public Icon getIcon(@NotNull PsiElement psiElement, int flags) {
-        PsiFile containingFile = psiElement.getContainingFile();
-        if (checkImagePath(containingFile)) {
-            Image image;
-            try {
-                image = ImageLoader.loadFromStream(new BufferedInputStream(new FileInputStream(containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath())));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                throw new IllegalStateException("Error loading preview Icon - " + containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath());
-            }
+  public Icon getIcon(@NotNull PsiElement psiElement, int flags) {
+    PsiFile containingFile = psiElement.getContainingFile();
+    if (checkImagePath(containingFile)) {
+      Image image;
+      final String canonicalPath = containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath();
+      try {
+        image = ImageLoader.loadFromStream(new BufferedInputStream(new FileInputStream(canonicalPath)));
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        throw new IllegalStateException("Error loading preview Icon - " + containingFile.getVirtualFile().getCanonicalFile()
+                                                                                        .getCanonicalPath());
+      }
 
-            if (image != null) {
-                return new ImageIcon(image.getScaledInstance(IMG_WIDTH, IMG_HEIGHT, Image.SCALE_SMOOTH));
-            }
-        }
-        return null;
+      if (image != null) {
+        return IconUtil.toSize(IconUtil.createImageIcon(image), IMG_WIDTH, IMG_HEIGHT);
+      }
     }
+    return null;
+  }
 
-    private boolean checkImagePath(PsiFile containingFile) {
-        return containingFile != null && containingFile.getVirtualFile() != null && containingFile.getVirtualFile().getCanonicalFile() != null && containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath() != null && UIUtils.isImageFile(containingFile.getName()) && ! containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath().contains(".jar");
-    }
+  private boolean checkImagePath(PsiFile containingFile) {
+    return containingFile != null && containingFile.getVirtualFile() != null && containingFile.getVirtualFile().getCanonicalFile() !=
+        null && containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath() != null && UIUtils.isImageFile(containingFile
+        .getName()) && !containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath().contains(".jar");
+  }
 }
